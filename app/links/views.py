63d6ac2c2b807ef99561aae8a670040b6fc5d844
arupakan125 <43802291+arupakan125@users.noreply.github.com>
2023-02-10 00:19:04 +0900

    Add redirect view

--- app/links/views.py
+++ app/links/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, get_object_or_404
+from links.models import Link
 # Create your views here.
+
+def redirect_view(request, pk):
+    link_object = get_object_or_404(Link, pk=pk)
+    return redirect(link_object.original_url)
--- app/shortener/urls.py
+++ app/shortener/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from links.views import redirect_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('<slug:pk>', redirect_view)
 ]
