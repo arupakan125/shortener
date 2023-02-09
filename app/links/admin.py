d035682535a80233ca134be10b891e18c4b8fc07
arupakan125 <43802291+arupakan125@users.noreply.github.com>
2023-02-10 00:10:23 +0900

    Add model that stores shorten links

--- app/links/admin.py
+++ app/links/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Link
+
 # Register your models here.
+
+@admin.register(Link)
+class AdminLink(admin.ModelAdmin):
+    readonly_fields = ('id', 'created_at', 'updated_at')
+    list_display = ('id', 'original_url', 'owner', 'created_at')
+    fields = ('id', 'original_url', 'owner', 'updated_at', 'created_at')
--- app/links/models.py
+++ app/links/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+from accounts.models import User
 
 # Create your models here.
+
+class Link(models.Model):
+    id = ShortUUIDField(
+        length=4,
+        primary_key=True,
+    )
+    original_url = models.CharField(max_length=1000)
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
